21a3f392161bd02866ddf8c3aaea43a21b70f70c
GuilhermeFiani <gfiani10@gmail.com>
2023-03-08 14:34:04 -0400

    Adiciona o modo de intervalo

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var startButton = document.getElementById("btnStart")
 var pauseButton = document.getElementById("btnPause")
+var interval = document.getElementById("interval")
+let duration = 60*25
+let delay = 60*5
 let pause
+let contPause = 0
 
 function startTimer(duration, display){
     var timer = duration, minutes, seconds
@@ -9,27 +13,38 @@ function startTimer(duration, display){
         minutes = parseInt(timer / 60 , 10) 
         seconds = parseInt(timer % 60 , 10)
 
-        minutes = minutes < 10 ? "0" + minutes : minutes
+        minutes = minutes < 10 ? "0" + minutes : minutes 
         seconds = seconds < 10 ? "0" + seconds : seconds
 
         display.textContent = minutes + ":" + seconds
 
         if(--timer < 0){
-            timer = duration
+            clearInterval(pause) 
         }
-    
+        
+        if(timer-1 < 0){ 
+            if (interval.checked){
+                timer = delay
+                contPause += 1 
+                if (timer-1 == 0 && contPause == 1){
+                    timer = duration
+                    contPause = 0
+                    console.log('teste')
+                }
+            }   
+        }
     }, 1000);
-}
+} 
 
 startButton.addEventListener('click', function(){
-    var duration = 60 * 25
     var display = document.querySelector("#timer")
     startTimer(duration, display)
     startButton.disabled = true
+    if (timer == 0){
+        startButton.disabled = false
+    }  
 })
 
 function pauseTimer(){
     clearInterval(pause)
-    duration = timer
-    
 }
